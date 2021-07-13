f5e6583917cc089c925b4db3ad9f7cabe99b3416
Alex <azakotyansky@gmail.com>
2021-07-13 16:27:27 -0400

    Initial commit

---
+++ src/main/java/alex/webapp/AplDashboardApplication.java
@@ -0,0 +1,13 @@
+package alex.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplDashboardApplication.class, args);
+	}
+
+}
